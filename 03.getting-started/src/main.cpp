4ed59d39b797ce6faa1b1c202ce34b076246ed83
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2026-02-27 14:35:06 -0600

    feat(03.gettting-started): add challenge solution

---
+++ 03.getting-started/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int number;
+    cout << "Enter a number between 1 and 100: ";
+    cin >> number;
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No really!!, " << number << " is my favorite number!" << endl;
+    return 0;
+}
