3be47e6b11a11c211886845dc894409bd734e1c5
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2026-03-18 10:20:31 -0600

    chore(constants): update comment

--- 06.variables-and-constants/src/constants/main.cpp
+++ 06.variables-and-constants/src/constants/main.cpp
@@ -3,5 +3,5 @@
 // in C++, we can declare constants using the "const" keyword
 // when we declare a constant, we must initialize it with a value at the time of declaration, and we cannot change that value later in the program
 // constants can be of any data type, including built-in types like int, double, char, and user-defined types like classes and structs
-// using constants can help improve the readability and maintainability of our code, as it makes it clear that certain values are meant to be constant and should
+// using constants can help improve the readability and maintainability of our code, as it makes it clear that certain values are meant to be constant and should not be modified
 const double pi = 3.14159;
