cba432ca0e4a341548767dc9fbfe3726d6668ed3
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2026-03-18 11:27:49 -0600

    feat(variables-constants): add explanation about numerical variables

--- 06.variables-and-constants/src/main.cpp
+++ 06.variables-and-constants/src/main.cpp
@@ -37,6 +37,95 @@ int main() {
      * third part.
      * In this part, we are using the constant "pi" that we defined in the "constants/main.cpp" file.
      */
-    cout << "The value of pi is: " << pi << endl;
+    // cout << "The value of pi is: " << pi << endl;
+
+    /**
+     * fourth part. In this part, we are declaring a variable "middle_initial" of type char, and we are initializing it with the value 'A'.
+     * We are then printing the value of "middle_initial" to the console. This part demonstrates how to declare and initialize a variable of type char, which is used to store a single character
+     */
+    char middle_initial {'A'};
+    cout << "My middle initial is: " << middle_initial << endl;
+
+    /**
+     * fifth part. In this part, we are declaring a variable "exam_score" of type unsigned short int, and we are initializing it with the value 55.
+     * We are then printing the value of "exam_score" to the console. This part demonstrates how to declare and initialize a variable of type unsigned short int, which is used to store a
+     */
+    unsigned short int exam_score {55};
+    cout << "My exam score is: " << exam_score << endl;
+
+    /**
+     * sixth part. In this part, we are declaring a variable "countries_represented" of type int, and we are initializing it with the value 65.
+     * We are then printing the value of "countries_represented" to the console. This part demonstrates how to declare and initialize a variable of type int, which is used to store whole
+     */
+    int countries_represented {65};
+    cout << "There are " << countries_represented << " countries represented in this meeting." << endl;
+
+    /**
+     * seventh part. In this part, we are declaring a variable "people_in_arizona" of type long, and we are initializing it with the value 7000000.
+     * We are then printing the value of "people_in_arizona" to the console. This part demonstrates how to declare and initialize a variable of type long, which is used to store larger
+     */
+    long people_in_arizona {7000000};
+    cout << "There are " << people_in_arizona << " people in Arizona." << endl;
+
+    /**
+     * eighth part. In this part, we are declaring a variable "people_on_earth" of type long, and we are initializing it with the value 8'000'000'000.
+     * We are then printing the value of "people_on_earth" to the console. This part demonstrates how to declare and initialize a variable of type long, which is used to store larger
+     */
+    long people_on_earth {8'000'000'000};
+    cout << "There are " << people_on_earth << " people on Earth." << endl;
+
+    /**
+     * ninth part. In this part, we are declaring a variable "distance_to_alpha_centauri" of type long long, and we are initializing it with the value 9'461'000'000'000.
+     * We are then printing the value of "distance_to_alpha_centauri" to the console. This part demonstrates how to declare and initialize a variable of type long long, which is used to
+     */
+    long long distance_to_alpha_centauri {9'461'000'000'000};
+    cout << "The distance to Alpha Centauri is: " << distance_to_alpha_centauri << " kilometers." << endl;
+
+    /**
+     * tenth part. In this part, we are declaring a variable "car_payment" of type float, and we are initializing it with the value 401.23.
+     * We are then printing the value of "car_payment" to the console. This part demonstrates how to declare and initialize a variable of type float, which is used to store decimal
+     */
+    float car_payment {401.23};
+    cout << "My car payment is: " << car_payment << endl;
+
+    /**
+     * eleventh part. In this part, we are declaring a variable "pi_value" of type double, and we are initializing it with the value 3.14159.
+     * We are then printing the value of "pi_value" to the console. This part demonstrates how to declare and initialize a variable of type double, which is used to store decimal
+     * values with higher precision than float.
+     */
+    double pi_value {3.14159};
+    cout << "The value of pi is: " << pi_value << endl;
+
+    /**
+     * twelfth part. In this part, we are declaring a variable "large_amount" of type long double, and we are initializing it with the value 2.7e120.
+     * We are then printing the value of "large_amount" to the console. This part demonstrates how to declare and initialize a variable of type long double, which is used to store
+     * decimal values with even higher precision than double.
+     */
+    long double large_amount {2.7e120};
+    cout << "A large amount is: " << large_amount << endl;
+
+    /**
+     * thirteenth part. In this part, we are declaring a variable "game_over" of type bool, and we are initializing it with the value false.
+     * We are then printing the value of "game_over" to the console. This part demonstrates how to declare and initialize a variable of type bool, which is used to store boolean values (
+     */
+    bool game_over {false};
+    cout << "The value of game_over is: " << game_over << endl;
+
+    /**
+     * Overflow example. In this part, we are declaring two variables "small_number" and "large_number" of type short, and we are initializing them with the values 30000 and 1000, respectively.
+     * We are then calculating the sum of "small_number" and "large_number" and storing the result in a variable called "result". Finally, we are printing the value of "result
+     * This part demonstrates the concept of overflow, which occurs when we try to store a value that exceeds the maximum limit of a data type. In this case, the maximum value for a short is 32767, and when we add 30000 and 1000, we get 31000, which exceeds the maximum limit. As a result, we get an overflow, and the value of "result" will wrap around and give us an incorrect value. This is an important concept to understand when working with variables and data types in C++.
+     */
+
+    /**
+     * fourteenth part. In this part, we are declaring two variables "value1" and "value2" of type short, and we are initializing them with the values 30000 and 1000, respectively.
+     * We are then calculating the sum of "value1" and "value2" and storing the result in a variable called "sum". Finally, we are printing the value of "sum
+     */
+    short value1 {30000};
+    short value2 {1000};
+    short product {value1 * value2};
+
+    cout << "The product of " << value1 << " and " << value2 << " is: " << product << endl;
+    
     return 0;
 }
