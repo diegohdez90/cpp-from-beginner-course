57664859a9eeb843d82f8a88c796b43bcb46750d
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2026-08-08 16:51:42 -0600

    feat(pointers): initialize pointer

--- 12.pointers/src/main.cpp
+++ 12.pointers/src/main.cpp
@@ -6,4 +6,12 @@ int main() {
     cout << "Value: " << num << endl;
     cout << "Sizeof: " << sizeof num << endl;
     cout << "Address: " << &num << endl;
+
+    int *p;
+    cout << "Value: " << p << endl; // garbage value, not recommended
+    cout << "Sizeof: " << sizeof p << endl;
+    cout << "Address: " << &p << endl;
+
+    p = nullptr;
+    cout << "Value: " << p << endl; // now, p has a value
 }
